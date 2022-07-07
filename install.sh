721b602aa60a3e2516fc16538026aa90eb415fa3
astudentinearth <95227070+astudentinearth@users.noreply.github.com>
2022-07-07 19:15:15 +0300

    Create install script. Add polybar launch script. Create README

---
+++ install.sh
@@ -0,0 +1,22 @@
+echo ===Configuring i3===
+mkdir -p ~/.config/i3
+cp ./config ~/.config/i3/config
+
+echo ===Configuring polybar===
+mkdir -p ~/.config/polybar
+cp ./config-polybar ~/.config/polybar/config.ini
+cp ./launch.sh ~/.config/polybar/launch.sh
+chmod +x ~/.config/polybar/launch.sh
+
+echo ===Configuring rofi===
+mkdir -p ~/.config/rofi
+cp ./config.rasi ~/.config/rofi/config.rasi
+
+echo ===Configuring alacritty===
+cp ./alacritty.yml ~/.config/alacritty/alacritty.yml
+
+echo ===Configuring picom==
+mkdir -p ~/.config/picom/
+cp ./picom.conf ~/.config/picom/picom.conf
+
+echo Done. Restart i3.
