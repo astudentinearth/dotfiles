77a9354d1d5c1afd1f0ede26774257e71a5608c9
Burak <burak.yn.dev@gmail.com>
2025-05-01 20:37:11 +0300

    update configs
    added kitty and zen browser configs
    made neovim transparent
    changed bar color

--- hyprland-2025/install-deps.sh
+++ hyprland-2025/install-deps.sh
@@ -1,2 +1,7 @@
+# install utils
 sudo pacman -S pipewire wireplumber sof-firmware playerctl brightnessctl xdg-user-dirs qt6ct lxappearance kitty
-yay -S aur/hyprshot aur/hyprprop-git aur/hyprpolkitagent-git aur/xdg-desktop-portal-hyprland-git aur/hyprpaper-git --no-confirm
+yay -S aur/hyprshot aur/hyprprop-git aur/hyprpolkitagent-git aur/xdg-desktop-portal-hyprland-git aur/hyprpaper-git --noconfirm
+
+# install apps
+yay -Syu wofi ttf-fantasque-nerd bibata-cursor-theme-bin zen-browser-bin dolphin
+
