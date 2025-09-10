39963017438620b44d995734a03a4b7b9c6ac883
Burak Yeniçeri <burak.yn.dev@gmail.com>
2025-09-10 05:08:26 +0300

    Switch back to nvim-cmp, assign leader+shift+f to format

--- nvim/init.lua
+++ nvim/init.lua
@@ -19,15 +19,15 @@ require("bufferline").setup()
 require("lualine").setup()
 vim.cmd("NvimTreeOpen")
 vim.cmd("TransparentEnable")
-vim.g.mapleader=" "
+vim.g.mapleader = " "
 vim.filetype.add({
-  extension = {
-    yuck = "lisp",
-  },
+    extension = {
+        yuck = "lisp",
+    },
 })
 
 
 vim.keymap.set('n', '<leader>f', ':FzfLua files<CR>', { desc = 'Go to next tab' })
-
-
-
+vim.keymap.set('n', '<leader><S-f>', function()
+    vim.lsp.buf.format()
+end, { desc = "Format file" })
--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,6 +1,55 @@
-
 return {
-    "neoclide/coc.nvim"
+    'neovim/nvim-lspconfig',
+    dependencies = {
+        'williamboman/mason.nvim',
+        'williamboman/mason-lspconfig.nvim',
+        'hrsh7th/nvim-cmp',
+        'hrsh7th/cmp-nvim-lsp',
+        'L3MON4D3/LuaSnip',
+        'saadparwaiz1/cmp_luasnip',
+        'onsails/lspkind.nvim'
+    },
+    config = function()
+        require("mason").setup()
+        require("mason-lspconfig").setup({
+            -- List language servers you want to install
+            ensure_installed = { "lua_ls", "ts_ls", "jsonls" },
+        })
+
+        local lspconfig = require('lspconfig')
+        local cmp = require('cmp')
+        local capabilities = require('cmp_nvim_lsp').default_capabilities()
+        cmp.setup({
+            sources = {
+                { name = 'nvim_lsp' },
+                { name = 'luasnip' },
+                { name = 'buffer' },
+            },
+            mapping = cmp.mapping.preset.insert({
+                ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+                ['<C-f>'] = cmp.mapping.scroll_docs(4),
+                ['<C-Space>'] = cmp.mapping.complete(),
+                ['<C-e>'] = cmp.mapping.abort(),
+                ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+            }),
+            snippet = {
+                expand = function(args)
+                    require('luasnip').lsp_expand(args.body)
+                end,
+            },
+            formatting = {
+                format = function(entry, vim_item)
+                    if vim.tbl_contains({ 'path' }, entry.source.name) then
+                        local icon, hl_group = require('nvim-web-devicons').get_icon(entry:get_completion_item().label)
+                        if icon then
+                            vim_item.kind = icon
+                            vim_item.kind_hl_group = hl_group
+                            return vim_item
+                        end
+                    end
+                    return require('lspkind').cmp_format({mode="symbol", maxwidth=50, ellipsis_char="...", with_text=false})(entry, vim_item)
+                end
+            },
+        })
+    end,
 }
-
-
