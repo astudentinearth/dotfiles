c8306cf7d4c7316f083429fdab803ca03aac6ef5
Burak Yeniçeri <burak.yn.dev@gmail.com>
2025-09-13 04:06:14 +0300

    Add nvim surround

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -13,7 +13,7 @@ return {
         require("mason").setup()
         require("mason-lspconfig").setup({
             -- List language servers you want to install
-            ensure_installed = { "lua_ls", "ts_ls", "jsonls" },
+            ensure_installed = { "lua_ls", "ts_ls", "jsonls", "pyright" },
         })
 
         local lspconfig = require('lspconfig')
