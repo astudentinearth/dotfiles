eb51ff3d696f700b33a6937824ed374ec408d45e
Burak Yeniçeri <burak.yn.dev@gmail.com>
2025-12-21 15:11:18 +0300

    brewfile equivalent but arch packages

---
+++ install-tools.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# chaotic-aur has most of them so it's better enabled
+# https://aur.chaotic.cx
+
+paru -S fd eza lazygit pastel bat libqalculate tlrc ripgrep yazi ffmpeg htop neovim git kitty zen-browser-bin
+
+
